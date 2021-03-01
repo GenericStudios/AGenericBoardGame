f5ceec5fe2a726cc8116c14f64e9e7f26d179436
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-03-01 16:05:32 +0000

    Update Main.java
    Argh, fine. I'll unbreak the code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hii");
-        Hahah now you]'ve given me write access I'm going to ruin all the code.
     }
 }
